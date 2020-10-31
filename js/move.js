c1c07d486ac0d678ab9b732070b4399fd50c46d9
Холодилов Александр <shurikhol@mail.ru>
2020-10-31 17:52:01 +0300

    Доработка алгоритма перемещения пина

--- js/move.js
+++ js/move.js
@@ -14,6 +14,15 @@
       y: downEvt.clientY
     };
 
+    const isMousePointerOnMap = function (x, y) {
+      return (
+        window.data.RANGE_X[0] <= x - window.map.MAFFIN_MIDDLE &&
+        window.data.RANGE_X[1] >= x - window.map.MAFFIN_MIDDLE
+        &&
+        window.data.RANGE_Y[0] <= y - window.map.MAFFIN_MIDDLE &&
+        window.data.RANGE_Y[1] >= y - window.map.MAFFIN_MIDDLE);
+    };
+
     const onMouseMove = function (moveEvt) {
       moveEvt.preventDefault();
 
@@ -22,22 +31,22 @@
         y: startCoords.y - moveEvt.clientY
       };
 
-      startCoords = {
-        x: moveEvt.clientX,
-        y: moveEvt.clientY
-      };
-
       const nextX = window.map.mapPinMain.offsetLeft - shift.x;
       const nextY = window.map.mapPinMain.offsetTop - shift.y;
 
-      if (window.data.RANGE_X[0] <= nextX && window.data.RANGE_X[1] >= nextX + 2 * window.map.MAFFIN_MIDDLE
-        &&
-        window.data.RANGE_Y[0] <= nextY && window.data.RANGE_Y[1] >= nextY) {
+      const mousePoinetrOnMap = isMousePointerOnMap(
+          window.scrollX + moveEvt.clientX,
+          window.scrollY + moveEvt.clientY);
+
+      if (mousePoinetrOnMap) {
+        startCoords = {
+          x: moveEvt.clientX,
+          y: moveEvt.clientY
+        };
+
         window.map.mapPinMain.style.top = nextY + `px`;
         window.map.mapPinMain.style.left = nextX + `px`;
         window.form.adFormAddress.value = window.map.getAddress(nextX, nextY);
-      } else {
-        onMoueUp();
       }
     };
 
